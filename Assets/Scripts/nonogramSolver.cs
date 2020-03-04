9199bfda01f597e242d2bb67b6a3c1434c20b92c
Sebastian <sebas_alpizar@hotmail.com>
2020-03-03 18:46:42 -0600

    Initial Commit

---
+++ Assets/Scripts/nonogramSolver.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nonogramSolver : MonoBehaviour {
+    private List<List<int>> X = new List<List<int>>();
+    private List<List<int>> Y = new List<List<int>>();
+
+    // Start is called before the first frame update
+    void Start() {
+        string[] lines = System.IO.File.ReadAllLines(@"Assets\Scripts\input.txt");
+        
+        foreach (string line in lines) {
+            Debug.Log("\t" + line);
+
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
