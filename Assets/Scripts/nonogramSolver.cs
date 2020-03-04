372b6c7025a1db4919613d6f3566d8c33235162a
Sebastian <sebas_alpizar@hotmail.com>
2020-03-03 23:30:54 -0600

    Added clues matrices and base matrix for nonogram

--- Assets/Scripts/nonogramSolver.cs
+++ Assets/Scripts/nonogramSolver.cs
@@ -3,17 +3,70 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class nonogramSolver : MonoBehaviour {
+    private int height;
+    private int lenght;
+    private List<List<int>> baseNonogram = new List<List<int>>();
     private List<List<int>> X = new List<List<int>>();
     private List<List<int>> Y = new List<List<int>>();
+    private bool skip = false;
 
     // Start is called before the first frame update
     void Start() {
         string[] lines = System.IO.File.ReadAllLines(@"Assets\Scripts\input.txt");
-        
+
+        bool rows = true;
         foreach (string line in lines) {
-            Debug.Log("\t" + line);
+            if (!skip) {
+                skip = true;
+                string[] words = line.Split(',');
+                this.lenght = int.Parse(words[0].Trim());
+                this.height = int.Parse(words[1].Trim());
+            } else {
+                if (!line.Contains("FILAS")) {
+                    if (line.Contains("COLUMNAS")) {
+                        rows = false;
+                    } else {
+                        string[] words = line.Split(',');
+
+                        List<int> listToAdd = new List<int>();
+                        listToAdd.Add(0); //Initial Zero for completenes of row.
+                        foreach (string number in words) {
+                            listToAdd.Add(int.Parse(number.Trim()));
+                            //Debug.Log(int.Parse(number.Trim()));
+                        }
+
+                        //string test = "[";
+                        //foreach (int num in listToAdd) {
+                        //    test += num + ",";
+                        //}
+                        //test += "]";
+
+                        //Debug.Log(test);
 
 
+                        if (rows) {
+                            X.Add(listToAdd);
+                        } else {
+                            Y.Add(listToAdd);
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i<this.height; i++) {
+            List<int> row = new List<int>();
+            for (int j = 0; i < this.lenght; j++) {
+                row.Add(0);
+            }
+            //string test = "[";
+            //foreach (int num in row) {
+            //    test += num + ",";
+            //}
+            //test += "]";
+
+            //Debug.Log(test);
+            baseNonogram.Add(row);
         }
 
     }
