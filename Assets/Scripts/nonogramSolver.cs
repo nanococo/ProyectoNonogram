6b1a3544759990b436197b30c87de333fde4037e
JorgeGuty <jguty2001@gmail.com>
2020-03-10 16:26:13 -0600

    Advance 1 in the solver of the nonogram
    Finished Simple Boxes Methos
    Developed the structure of the solving method
    Updated structure of the logical matrix.

--- Assets/Scripts/nonogramSolver.cs
+++ Assets/Scripts/nonogramSolver.cs
@@ -16,6 +16,27 @@ public class nonogramSolver : MonoBehaviour {
         createCluesMatrix();
         createLogicalMatrix();
 
+        Board board = Board.MakeFooObject(this.height, this.length);
+
+        Debug.Log(this.length);
+        board.draw(cell);
+        solvePuzzle();
+        Debug.Log("X-CLUES");
+        printMatrix(X);
+        Debug.Log("X-CLUES");
+        Debug.Log("%%%%%%%%%%%%%%%%%%%%%%%%%");
+        Debug.Log("Y-CLUES");
+        printMatrix(Y);
+        Debug.Log("Y-CLUES");
+        Debug.Log("%%%%%%%%%%%%%%%%%%%%%%%%%");
+        Debug.Log("MATRIX");
+        printMatrix(baseNonogram);
+        Debug.Log("MATRIX");
+        // Debug.Log("Min and max cases");
+        // printList(getMinCase(Y[5],baseNonogram[0].Count));
+        // printList(getMaxCase(Y[5],baseNonogram[0].Count));
+        // Debug.Log("Result");
+        // printList(simpleBoxes(baseNonogram[0], Y[5]));
     }
 
     // Update is called once per frame
@@ -27,8 +48,8 @@ public class nonogramSolver : MonoBehaviour {
 
         for (int i = 0; i < this.height; i++) {
             List<int> row = new List<int>();
-            for (int j = 0; i < this.lenght; j++) {
-                row.Add(0);
+            for (int j = 0; j < this.length; j++) {
+                row.Add(9);
             }
             string test = "[";
             foreach (int num in row) {
@@ -61,21 +82,11 @@ public class nonogramSolver : MonoBehaviour {
                         string[] words = line.Split(',');
 
                         List<int> listToAdd = new List<int>();
-                        listToAdd.Add(0); //Initial Zero for completenes of row.
+                        //listToAdd.Add(0); //Initial Zero for completenes of row.
                         foreach (string number in words) {
                             listToAdd.Add(int.Parse(number.Trim()));
-                            Debug.Log(int.Parse(number.Trim()));
                         }
-
-                        string test = "[";
-                        foreach (int num in listToAdd) {
-                            test += num + ",";
-                        }
-                        test += "]";
-
-                        Debug.Log(test);
-
-
+                        
                         if (rows) {
                             X.Add(listToAdd);
                         }
@@ -88,13 +99,248 @@ public class nonogramSolver : MonoBehaviour {
         }
     }
 
+    void solvePuzzle(){
+        
+        Debug.Log("Rows");
+        goThroughRows();
+        Debug.Log("Collumns");
+        goThroughCollumns();
+       
+
+    }
+
+    void goThroughRows(){
+        for(int index = 0; index < height; index++){
+            analyzeLine(true, index);
+        }
+    }
+    
+    
+    void goThroughCollumns(){
+
+            analyzeLine(false, index);
+        }
+    
+    }
+
+    void analyzeLine(bool isRow, int index) {
+        List<int> lineBeingAnalyzed;
+        List<int> clues;
+        if (isRow) {
+            lineBeingAnalyzed = this.baseNonogram[index];
+            clues = getCluesByIndex(index, true);
+            
+        } else {
+            setColumnAccessList(index);
+            lineBeingAnalyzed = columnEasyAccessList;
+            clues = getCluesByIndex(index, false);
+
+        }
+
+        if (isUpToSimpleBoxes(lineBeingAnalyzed, clues)) {
+            lineBeingAnalyzed = simpleBoxes(lineBeingAnalyzed, clues);
+            printList(lineBeingAnalyzed);
+            copyResultIntoNonogram(isRow, index, lineBeingAnalyzed);
+        }
+        
+    }
+
+    void copyResultIntoNonogram(bool isRow, int index, List<int> lineProcessed){
+
+        if(isRow){
+
+            baseNonogram[index] = lineProcessed;
+
+        } else{
+
+            copyResultIntoCollumn(index, lineProcessed);
+
+        }
+
+    }
+
+    void copyResultIntoCollumn(int index, List<int> lineProcessed){
+        
+        for(int nonogramIndex = 0; nonogramIndex < baseNonogram.Count; nonogramIndex++){
+            baseNonogram[nonogramIndex][index] = lineProcessed[nonogramIndex];
+        }
+
+    }
+
    
-    void simpleBoxes() {
+    List<int> simpleBoxes(List<int> lineBeingAnalyzed, List<int> clues) {
+
+        int lineSize = lineBeingAnalyzed.Count;
+
+        List<int> minCase = getMinCase(clues, lineSize);
+        List<int> maxCase = getMaxCase(clues, lineSize);
+
+        List<int> simpleBoxesResult = commonConfirmedCellsBetweenLines(minCase, maxCase);
+
+        return simpleBoxesResult;
+    
+    }
+
+
+
+    List<int> getMinCase(List<int> clues, int lineSize) {
+
+        List<int> minCase = getMinClueDistribution(clues, lineSize);
+
+        while(minCase.Count < lineSize){
+            minCase.Add(9);
+        }
+        return minCase;
+    
+    }
+
+    List<int> getMaxCase(List<int> clues, int lineSize) {
+
+        List<int> maxCase = getMinClueDistribution(clues, lineSize);
+        int maxSapcesBeforeClues = getMaxSpacesBeforeClues(clues, lineSize);
+
+        
+        while(maxCase.Count < lineSize){
+            maxCase.Insert(0,9);
+            maxSapcesBeforeClues--;
+        }
+
+       return maxCase;
 
     }
-    void simpleSpaces() {
+
+    List<int> getMinClueDistribution(List<int> clues, int lineSize){
+        
+        List<int> minClueDistribution = new List<int> {};
+        int cluesQuantity = clues.Count;
+        
+        for (int clueIndex = 0; clueIndex < cluesQuantity; clueIndex++){
+            
+            for(int clueCounter = 0; clueCounter < clues[clueIndex]; clueCounter++){
+                minClueDistribution.Add(1);
+            }
+            if(clueIndex < clues.Count-1){
+                minClueDistribution.Add(9);
+            }    
+        
+        }
+        
+        return minClueDistribution;
 
     }
 
+    bool isUpToSimpleBoxes(List<int> lineBeingAnalyzed, List<int> clues) {
+        
+        int sumOfClues = addListElements(clues);
+        if (sumOfClues > (lineBeingAnalyzed.Count / 2) && isAnEmptyLine(lineBeingAnalyzed)) { 
+            return true;
+        }
+        else return false;
+    
+    }
+
+    int getMaxSpacesBeforeClues(List<int> clues, int listSize){
+
+        int sumOfClues = addListElements(clues);
+        int conditionSum = sumOfClues + getObligatorySpacesBetweenClues(clues);
+        int maxSapcesBeforeClues = listSize - conditionSum;
+        
+        return maxSapcesBeforeClues;
+
+    }
+
+    int addListElements(List<int> list) {
+        int sum = 0;        
+        foreach (int element in list){
+            sum += element;
+        }
+        return sum;
+    }
+
+
+
+
+    int getObligatorySpacesBetweenClues(List<int> clues) {
+        return clues.Count - 1; 
+    }
+
+
+    bool isAnEmptyLine(List<int> line) {
+        bool isEmpty = true;
+        foreach (int element in line) {
+            if (element != 9) isEmpty = false;
+        }
+        return isEmpty;
+    }
+
+    List<int> getCluesByIndex(int index, bool isRow) {
+     
+        if (isRow) {
+            return X[index];
+        }
+        else return Y[index];
+    
+    }
+    
+
+    void markCell(int xIndex, int yIndex, int oneOrZero) { //0 marks discarded, 1 marks confirmed
+        this.baseNonogram[xIndex][yIndex] = oneOrZero;
+    }
+
+    void setColumnAccessList(int collumnIndex) { //Sets the values of the column access list with the given collumn index
+        this.columnEasyAccessList.Clear();
+        foreach (List<int> row in this.baseNonogram) {
+            for (int cellIndex = 0; cellIndex < this.length; cellIndex++) {
+                if (cellIndex == collumnIndex) {
+                    this.columnEasyAccessList.Add(row[cellIndex]);
+                }
+            }
+        }
+        
+    }
+
+    List<int> commonConfirmedCellsBetweenLines(List<int> lineA, List<int> lineB){ //Lines the same size
+
+       List<int> commonConfirmedCells = new List<int> {};
+
+        for (int index = 0; index < lineA.Count; index++){
+            
+            if (lineA[index] == 1 && lineB[index] == 1){
+                commonConfirmedCells.Add(1);
+            }
+            else{
+                commonConfirmedCells.Add(9);
+            }
+
+        }
+        return commonConfirmedCells;
+       
+    }
+
+    bool isEvenSize(List<int> list){
+        if (list.Count % 2 == 0){
+            return true;
+        } else return false;
+    }
+
+    void printMatrix(List<List<int>> matrix) {
+        foreach (List<int> row in matrix) {
+            string test = "[";
+            foreach (int cell in row) {
+                test += cell + ",";
+            }
+            test += "]";
+            Debug.Log(test);
+        }
+    }
+    
+    void printList(List<int> list) {
+        string test = "[";
+        foreach (int cell in list) {
+            test += cell + ",";
+        }
+        test += "]";
+        Debug.Log(test);
+    }
 
 }
